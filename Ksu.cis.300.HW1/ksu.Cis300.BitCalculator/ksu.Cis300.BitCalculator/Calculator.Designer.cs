80141f1b4639586afb57a61345bd8c312dd9a584
deadDrifters <nebyoung@ksu.edu>
2016-09-14 17:00:29 -0500

    Add files via upload

---
+++ Ksu.cis.300.HW1/ksu.Cis300.BitCalculator/ksu.Cis300.BitCalculator/Calculator.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace ksu.Cis300.BitCalculator
+{
+    partial class UxCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UxD = new System.Windows.Forms.Button();
+            this.UxE = new System.Windows.Forms.Button();
+            this.UxF = new System.Windows.Forms.Button();
+            this.UxClear = new System.Windows.Forms.Button();
+            this.UxAND = new System.Windows.Forms.Button();
+            this.UxC = new System.Windows.Forms.Button();
+            this.UxB = new System.Windows.Forms.Button();
+            this.UxA = new System.Windows.Forms.Button();
+            this.UxXOR = new System.Windows.Forms.Button();
+            this.Ux6 = new System.Windows.Forms.Button();
+            this.Ux5 = new System.Windows.Forms.Button();
+            this.Ux4 = new System.Windows.Forms.Button();
+            this.UxOR = new System.Windows.Forms.Button();
+            this.Ux9 = new System.Windows.Forms.Button();
+            this.Ux8 = new System.Windows.Forms.Button();
+            this.Ux7 = new System.Windows.Forms.Button();
+            this.UxClosePar = new System.Windows.Forms.Button();
+            this.UxOpenPar = new System.Windows.Forms.Button();
+            this.Ux0 = new System.Windows.Forms.Button();
+            this.UxEquals = new System.Windows.Forms.Button();
+            this.UxNOT = new System.Windows.Forms.Button();
+            this.Ux3 = new System.Windows.Forms.Button();
+            this.Ux2 = new System.Windows.Forms.Button();
+            this.Ux1 = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UxD
+            // 
+            this.UxD.Location = new System.Drawing.Point(12, 57);
+            this.UxD.Name = "UxD";
+            this.UxD.Size = new System.Drawing.Size(54, 34);
+            this.UxD.TabIndex = 0;
+            this.UxD.Text = "D";
+            this.UxD.UseVisualStyleBackColor = true;
+            this.UxD.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxE
+            // 
+            this.UxE.Location = new System.Drawing.Point(72, 57);
+            this.UxE.Name = "UxE";
+            this.UxE.Size = new System.Drawing.Size(54, 34);
+            this.UxE.TabIndex = 1;
+            this.UxE.Text = "E";
+            this.UxE.UseVisualStyleBackColor = true;
+            this.UxE.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxF
+            // 
+            this.UxF.Location = new System.Drawing.Point(132, 57);
+            this.UxF.Name = "UxF";
+            this.UxF.Size = new System.Drawing.Size(54, 34);
+            this.UxF.TabIndex = 2;
+            this.UxF.Text = "F";
+            this.UxF.UseVisualStyleBackColor = true;
+            this.UxF.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxClear
+            // 
+            this.UxClear.Location = new System.Drawing.Point(192, 57);
+            this.UxClear.Name = "UxClear";
+            this.UxClear.Size = new System.Drawing.Size(54, 34);
+            this.UxClear.TabIndex = 3;
+            this.UxClear.Text = "Clear";
+            this.UxClear.UseVisualStyleBackColor = true;
+            this.UxClear.Click += new System.EventHandler(this.UxClear_Click);
+            // 
+            // UxAND
+            // 
+            this.UxAND.Location = new System.Drawing.Point(192, 97);
+            this.UxAND.Name = "UxAND";
+            this.UxAND.Size = new System.Drawing.Size(54, 34);
+            this.UxAND.TabIndex = 7;
+            this.UxAND.Text = "AND";
+            this.UxAND.UseVisualStyleBackColor = true;
+            this.UxAND.Click += new System.EventHandler(this.UxAND_Click);
+            // 
+            // UxC
+            // 
+            this.UxC.Location = new System.Drawing.Point(132, 97);
+            this.UxC.Name = "UxC";
+            this.UxC.Size = new System.Drawing.Size(54, 34);
+            this.UxC.TabIndex = 6;
+            this.UxC.Text = "C";
+            this.UxC.UseVisualStyleBackColor = true;
+            this.UxC.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxB
+            // 
+            this.UxB.Location = new System.Drawing.Point(72, 97);
+            this.UxB.Name = "UxB";
+            this.UxB.Size = new System.Drawing.Size(54, 34);
+            this.UxB.TabIndex = 5;
+            this.UxB.Text = "B";
+            this.UxB.UseVisualStyleBackColor = true;
+            this.UxB.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxA
+            // 
+            this.UxA.Location = new System.Drawing.Point(12, 97);
+            this.UxA.Name = "UxA";
+            this.UxA.Size = new System.Drawing.Size(54, 34);
+            this.UxA.TabIndex = 4;
+            this.UxA.Text = "A";
+            this.UxA.UseVisualStyleBackColor = true;
+            this.UxA.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxXOR
+            // 
+            this.UxXOR.Location = new System.Drawing.Point(192, 177);
+            this.UxXOR.Name = "UxXOR";
+            this.UxXOR.Size = new System.Drawing.Size(54, 34);
+            this.UxXOR.TabIndex = 15;
+            this.UxXOR.Text = "XOR";
+            this.UxXOR.UseVisualStyleBackColor = true;
+            this.UxXOR.Click += new System.EventHandler(this.UxAND_Click);
+            // 
+            // Ux6
+            // 
+            this.Ux6.Location = new System.Drawing.Point(132, 177);
+            this.Ux6.Name = "Ux6";
+            this.Ux6.Size = new System.Drawing.Size(54, 34);
+            this.Ux6.TabIndex = 14;
+            this.Ux6.Text = "6";
+            this.Ux6.UseVisualStyleBackColor = true;
+            this.Ux6.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux5
+            // 
+            this.Ux5.Location = new System.Drawing.Point(72, 177);
+            this.Ux5.Name = "Ux5";
+            this.Ux5.Size = new System.Drawing.Size(54, 34);
+            this.Ux5.TabIndex = 13;
+            this.Ux5.Text = "5";
+            this.Ux5.UseVisualStyleBackColor = true;
+            this.Ux5.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux4
+            // 
+            this.Ux4.Location = new System.Drawing.Point(12, 177);
+            this.Ux4.Name = "Ux4";
+            this.Ux4.Size = new System.Drawing.Size(54, 34);
+            this.Ux4.TabIndex = 12;
+            this.Ux4.Text = "4";
+            this.Ux4.UseVisualStyleBackColor = true;
+            this.Ux4.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxOR
+            // 
+            this.UxOR.Location = new System.Drawing.Point(192, 137);
+            this.UxOR.Name = "UxOR";
+            this.UxOR.Size = new System.Drawing.Size(54, 34);
+            this.UxOR.TabIndex = 11;
+            this.UxOR.Text = "OR";
+            this.UxOR.UseVisualStyleBackColor = true;
+            this.UxOR.Click += new System.EventHandler(this.UxAND_Click);
+            // 
+            // Ux9
+            // 
+            this.Ux9.Location = new System.Drawing.Point(132, 137);
+            this.Ux9.Name = "Ux9";
+            this.Ux9.Size = new System.Drawing.Size(54, 34);
+            this.Ux9.TabIndex = 10;
+            this.Ux9.Text = "9";
+            this.Ux9.UseVisualStyleBackColor = true;
+            this.Ux9.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux8
+            // 
+            this.Ux8.Location = new System.Drawing.Point(72, 137);
+            this.Ux8.Name = "Ux8";
+            this.Ux8.Size = new System.Drawing.Size(54, 34);
+            this.Ux8.TabIndex = 9;
+            this.Ux8.Text = "8";
+            this.Ux8.UseVisualStyleBackColor = true;
+            this.Ux8.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux7
+            // 
+            this.Ux7.Location = new System.Drawing.Point(12, 137);
+            this.Ux7.Name = "Ux7";
+            this.Ux7.Size = new System.Drawing.Size(54, 34);
+            this.Ux7.TabIndex = 8;
+            this.Ux7.Text = "7";
+            this.Ux7.UseVisualStyleBackColor = true;
+            this.Ux7.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxClosePar
+            // 
+            this.UxClosePar.Location = new System.Drawing.Point(192, 257);
+            this.UxClosePar.Name = "UxClosePar";
+            this.UxClosePar.Size = new System.Drawing.Size(54, 34);
+            this.UxClosePar.TabIndex = 23;
+            this.UxClosePar.Text = ")";
+            this.UxClosePar.UseVisualStyleBackColor = true;
+            this.UxClosePar.Click += new System.EventHandler(this.UxClosePar_Click);
+            // 
+            // UxOpenPar
+            // 
+            this.UxOpenPar.Location = new System.Drawing.Point(132, 257);
+            this.UxOpenPar.Name = "UxOpenPar";
+            this.UxOpenPar.Size = new System.Drawing.Size(54, 34);
+            this.UxOpenPar.TabIndex = 22;
+            this.UxOpenPar.Text = "(";
+            this.UxOpenPar.UseVisualStyleBackColor = true;
+            // 
+            // Ux0
+            // 
+            this.Ux0.Location = new System.Drawing.Point(72, 257);
+            this.Ux0.Name = "Ux0";
+            this.Ux0.Size = new System.Drawing.Size(54, 34);
+            this.Ux0.TabIndex = 21;
+            this.Ux0.Text = "0";
+            this.Ux0.UseVisualStyleBackColor = true;
+            this.Ux0.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // UxEquals
+            // 
+            this.UxEquals.Location = new System.Drawing.Point(12, 257);
+            this.UxEquals.Name = "UxEquals";
+            this.UxEquals.Size = new System.Drawing.Size(54, 34);
+            this.UxEquals.TabIndex = 20;
+            this.UxEquals.Text = "=";
+            this.UxEquals.UseVisualStyleBackColor = true;
+            // 
+            // UxNOT
+            // 
+            this.UxNOT.Location = new System.Drawing.Point(192, 217);
+            this.UxNOT.Name = "UxNOT";
+            this.UxNOT.Size = new System.Drawing.Size(54, 34);
+            this.UxNOT.TabIndex = 19;
+            this.UxNOT.Text = "NOT";
+            this.UxNOT.UseVisualStyleBackColor = true;
+            this.UxNOT.Click += new System.EventHandler(this.UxNOT_Click);
+            // 
+            // Ux3
+            // 
+            this.Ux3.Location = new System.Drawing.Point(132, 217);
+            this.Ux3.Name = "Ux3";
+            this.Ux3.Size = new System.Drawing.Size(54, 34);
+            this.Ux3.TabIndex = 18;
+            this.Ux3.Text = "3";
+            this.Ux3.UseVisualStyleBackColor = true;
+            this.Ux3.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux2
+            // 
+            this.Ux2.Location = new System.Drawing.Point(72, 217);
+            this.Ux2.Name = "Ux2";
+            this.Ux2.Size = new System.Drawing.Size(54, 34);
+            this.Ux2.TabIndex = 17;
+            this.Ux2.Text = "2";
+            this.Ux2.UseVisualStyleBackColor = true;
+            this.Ux2.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // Ux1
+            // 
+            this.Ux1.Location = new System.Drawing.Point(12, 217);
+            this.Ux1.Name = "Ux1";
+            this.Ux1.Size = new System.Drawing.Size(54, 34);
+            this.Ux1.TabIndex = 16;
+            this.Ux1.Text = "1";
+            this.Ux1.UseVisualStyleBackColor = true;
+            this.Ux1.Click += new System.EventHandler(this.Ux0_Click_1);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 21);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.ReadOnly = true;
+            this.TextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TextBox.Size = new System.Drawing.Size(234, 20);
+            this.TextBox.TabIndex = 24;
+            this.TextBox.Text = "0";
+            this.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // UxCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 324);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.UxClosePar);
+            this.Controls.Add(this.UxOpenPar);
+            this.Controls.Add(this.Ux0);
+            this.Controls.Add(this.UxEquals);
+            this.Controls.Add(this.UxNOT);
+            this.Controls.Add(this.Ux3);
+            this.Controls.Add(this.Ux2);
+            this.Controls.Add(this.Ux1);
+            this.Controls.Add(this.UxXOR);
+            this.Controls.Add(this.Ux6);
+            this.Controls.Add(this.Ux5);
+            this.Controls.Add(this.Ux4);
+            this.Controls.Add(this.UxOR);
+            this.Controls.Add(this.Ux9);
+            this.Controls.Add(this.Ux8);
+            this.Controls.Add(this.Ux7);
+            this.Controls.Add(this.UxAND);
+            this.Controls.Add(this.UxC);
+            this.Controls.Add(this.UxB);
+            this.Controls.Add(this.UxA);
+            this.Controls.Add(this.UxClear);
+            this.Controls.Add(this.UxF);
+            this.Controls.Add(this.UxE);
+            this.Controls.Add(this.UxD);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "UxCalculator";
+            this.Text = "Hex Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UxD;
+        private System.Windows.Forms.Button UxE;
+        private System.Windows.Forms.Button UxF;
+        private System.Windows.Forms.Button UxClear;
+        private System.Windows.Forms.Button UxAND;
+        private System.Windows.Forms.Button UxC;
+        private System.Windows.Forms.Button UxB;
+        private System.Windows.Forms.Button UxA;
+        private System.Windows.Forms.Button UxXOR;
+        private System.Windows.Forms.Button Ux6;
+        private System.Windows.Forms.Button Ux5;
+        private System.Windows.Forms.Button Ux4;
+        private System.Windows.Forms.Button UxOR;
+        private System.Windows.Forms.Button Ux9;
+        private System.Windows.Forms.Button Ux8;
+        private System.Windows.Forms.Button Ux7;
+        private System.Windows.Forms.Button UxClosePar;
+        private System.Windows.Forms.Button UxOpenPar;
+        private System.Windows.Forms.Button Ux0;
+        private System.Windows.Forms.Button UxEquals;
+        private System.Windows.Forms.Button UxNOT;
+        private System.Windows.Forms.Button Ux3;
+        private System.Windows.Forms.Button Ux2;
+        private System.Windows.Forms.Button Ux1;
+        private System.Windows.Forms.TextBox TextBox;
+    }
+}
+
---
+++ Ksu.cis.300.HW1/ksu.Cis300.BitCalculator/ksu.Cis300.BitCalculator/Calculator.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ksu.Cis300.BitCalculator
+{
+    public partial class UxCalculator : Form
+    {
+
+        private Stack _wholeExpression = new Stack();
+        private bool _inputMode = true;
+        public UxCalculator()
+        {
+            InitializeComponent();
+        }
+
+        //good
+        private void Ux0_Click_1(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string input = btn.Text;
+            if (!_inputMode)
+            {
+                TextBox.Text = input;
+                _inputMode = true;
+            }
+            else if (_inputMode) {
+                if (TextBox.Text.Length >= 8)
+                {
+                    return;
+                }
+                else if (TextBox.Text == "0")
+                {
+                    TextBox.Text = input;
+                }
+                else
+                {
+                    TextBox.Text += input;
+                }
+             }   
+        }
+        
+        //good
+        private int Precendence(string _inString)
+        {
+            int _precedenceLvl = 0;
+            switch (_inString)
+            {
+                case "NOT":
+                    _precedenceLvl = 5;
+                    break;
+                case "AND":
+                    _precedenceLvl = 4;
+                    break;
+                case "XOR":
+                    _precedenceLvl = 3;
+                    break;
+                case "OR":
+                    _precedenceLvl = 2;
+                    break;
+                case ")":
+                    _precedenceLvl = 2;
+                    break;
+                case "(":
+                    _precedenceLvl = 1;
+                    break;
+                default:
+                    _precedenceLvl = 1000;
+                    break;
+            }
+            return _precedenceLvl;
+        }
+
+        //good
+        private uint BinaryOp(string _inString, uint _operandOne, uint _operandTwo)
+        {
+            uint _result = 0;
+            switch (_inString)
+            {
+                case "AND":
+                    _result = _operandOne & _operandTwo;
+                    break;
+                case "XOR":
+                    _result = _operandOne ^ _operandTwo;
+                    break;
+                case "OR":
+                    _result = _operandOne | _operandTwo;
+                    break;                
+            }
+            return _result;
+        }
+
+        private uint Simplifier(int minPrecendence, uint operand)
+        {
+            uint simpleExp = operand;
+            if (_wholeExpression.Count == 0)
+            {
+                return simpleExp;
+            }
+            else while (_wholeExpression.Count > 0 && Precendence(_wholeExpression.Peek().ToString()) >= minPrecendence)
+            {
+                simpleExp = BinaryOp(_wholeExpression.Pop().ToString(), Convert.ToUInt32(_wholeExpression.Pop()), simpleExp);
+            }
+            return simpleExp;
+        }
+
+        private uint ClosePar(uint newValue)
+        {
+            uint simpleExp = newValue;
+                do
+                {
+                    simpleExp = BinaryOp(_wholeExpression.Pop().ToString(), Convert.ToUInt32(_wholeExpression.Pop()), simpleExp);
+                    //need error code
+                } while (_wholeExpression.Peek().ToString() != "(");
+            if (_wholeExpression.Peek().ToString() == "(")
+            {
+                _wholeExpression.Pop();
+            }
+            return simpleExp;
+        }
+
+        //good
+        private void UxNOT_Click(object sender, EventArgs e)
+        {
+            uint expression = Convert.ToUInt32(TextBox.Text, 16);
+            expression = expression ^ 0xFFFFFFFF;
+            TextBox.Text = expression.ToString("X");
+            _inputMode = false;
+        }
+
+        private void UxAND_Click(object sender, EventArgs e)
+        {
+            
+            //_wholeExpression.Push(TextBox.Text);
+            int _precedence = Precendence((sender as Button).Text);
+            uint evaluated = Simplifier(_precedence, Convert.ToUInt32(TextBox.Text));
+            TextBox.Text = evaluated.ToString();
+            _wholeExpression.Push(evaluated);
+            _inputMode = false;
+        }
+
+        private void UxClosePar_Click(object sender, EventArgs e)
+        {
+            uint paranthesis = ClosePar(Convert.ToUInt32((sender as Button).Text));
+            TextBox.Text = paranthesis.ToString();
+            _inputMode = false;
+        }
+
+        //good
+        private void UxClear_Click(object sender, EventArgs e)
+        {
+            if (_inputMode)
+            {
+                TextBox.Text = "0";
+                _inputMode = false;
+            }
+            else if (!_inputMode)
+            {
+                TextBox.Text = "0";
+                while (_wholeExpression.Count > 0)
+                {
+                    _wholeExpression.Pop();
+                }
+                _inputMode = true;
+            }
+        }
+
+
+    }
+}
